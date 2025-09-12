ff1711dfa8e8a5b07dd6443d03aec47abb3d7e47
Arthur Ransolin <arthur12022005ransolin@gmail.com>
2025-09-12 19:20:06 -0300

    envio certo da primeira aula

---
+++ src/telas/Home/Componentes/Topo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Image, View, Text} from "react-native";
+
+import logo from "../../../assets/logo.png";
+
+export default function Topo() {
+  return(
+  <View>
+    <Image source = {logo} />
+    <Text>Olá, Arthur!</Text>
+    <Text>Encontre os melhores produtores.</Text>
+  </View>
+  );
+}
+
+
+
---
+++ src/telas/Home/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Topo from "./Componentes/Topo.js";
+
+export default function Home() {
+  return <Topo/>;
+}
+
+
+
